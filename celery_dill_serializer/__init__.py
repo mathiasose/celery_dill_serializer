20dc2968824f41c0fc414d8b26fd606f07243240
Mathias Ose <mathias.ose@gmail.com>
2017-01-11 21:54:34 +0100

    Add function to register dill as celery serializer

---
+++ celery_dill_serializer/__init__.py
@@ -0,0 +1,19 @@
+from dill import dill
+from kombu.serialization import pickle_loads, pickle_protocol, registry
+from kombu.utils.encoding import str_to_bytes
+
+
+def register_dill():
+    def encode(obj, dumper=dill.dumps):
+        return dumper(obj, protocol=pickle_protocol)
+
+    def decode(s):
+        return pickle_loads(str_to_bytes(s), load=dill.load)
+
+    registry.register(
+        name='dill',
+        encoder=encode,
+        decoder=decode,
+        content_type='application/x-python-serialize',
+        content_encoding='binary'
+    )
